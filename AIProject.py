a321626ff1b34fdd0105ace181db91083f9d0381
YuhL6 <49777375+YuhL6@users.noreply.github.com>
2019-12-25 16:53:43 +0800

    Add files via upload

---
+++ AIProject.py
@@ -0,0 +1,94 @@
+import cv2
+from api import load_image_file
+from api import face_encodings
+from api import face_locations
+from api import compare_faces
+import PIL.Image
+import numpy as np
+import threading
+import os
+
+
+def camera():
+    global dic
+    global counter
+    camera = cv2.VideoCapture(0)
+    cv2.namedWindow('MyCamera')
+    while True:
+        success, frame = camera.read()
+        locations = face_locations(frame)
+        faces = face_encodings(frame)
+        for i in range(0, len(locations)):
+            location = locations[i]
+            cv2.rectangle(frame, (location[3], location[0]), (location[1], location[2]), (255, 255, 255))
+            have_same = False
+            for key in dic:
+                if compare_faces([faces[i]], dic[key],tolerance=0.4)[0]:
+                    # print('existed face',str(key))
+                    cv2.putText(frame, str(key), (location[1], location[2]), font, font_scale, (255, 255, 255))
+                    have_same = True
+                    break
+            if not have_same:
+                counter += 1
+                # print('new face!')
+                dic[str(counter)] = faces[i]
+                # print(dic)
+                cv2.putText(frame, str(counter), (location[1], location[2]), font, font_scale, (255, 255, 255))
+        cv2.imshow('MyCamera', frame)
+        if cv2.waitKey(1) & 0xff == ord(' '):
+            break
+    cv2.destroyWindow('MyCamera')
+    camera.release()
+
+
+def readKey():
+    while True:
+        i = input()
+        global dic
+        if ':' in list(i):
+            old = i.split(':')[0]
+            new = i.split(':')[1]
+            if old in dic.keys():
+                dic[new]=dic[old]
+                dic.pop(old)
+            file = open('data', 'r+')
+            file.read()
+            kv=new+':'+str(dic[new])
+            # kv.replace('\n','')
+            # kv.replace('\r','')
+            file.writelines(kv)
+            file.writelines('***')
+            file.close()
+
+def readData(path):
+    global dic
+    file = open(path, 'r+')
+    data = file.read()
+    data = data.split('***')
+    print(data)
+    for d in data:
+        if d != '' and d != '\n':
+            d = d.split(':')
+            key = d[0]
+            value = []
+            d[1] = d[1].strip('[')
+            d[1] = d[1].strip(']')
+            d[1].replace('\n', '')
+            d[1] = d[1].split(' ')
+            for i in d[1]:
+                if i != '':
+                    i = float(i)
+                    value.append(i)
+            value=np.array(value)
+            dic[key] = value
+
+
+dic = {}
+readData('data')
+counter=0
+font = cv2.FONT_HERSHEY_SIMPLEX
+font_scale = 2
+task1 = threading.Thread(target=camera)
+task2 = threading.Thread(target=readKey)
+task1.start()
+task2.start()
